47c3a518f88629a9f301cb775d9a8c260d7dc602
hvanbelle <hvanbelle@gmail.com>
2015-04-10 15:11:05 +0200

    Update Enquiry.js

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -15,11 +15,15 @@ Enquiry.add({
 	name: { type: Types.Name, required: true },
 	email: { type: Types.Email, required: true },
 	phone: { type: String },
+	estatus: { type: String, default: 'active', required: true},
 	enquiryType: { type: Types.Select, options: [
 		{ value: '2015-04-01-fysiek-zwembadtraining-lebbeke', label: '2015-04-01 - Fysiek Zwembadtraining Lebbeke' },
 		{ value: '2015-04-02-fysiek-zwembadtraining-lebbeke', label: '2015-04-02 - Fysiek Zwembadtraining Lebbeke' },		
 		{ value: '2015-04-03-fysiek-zwembadtraining-lebbeke', label: '2015-04-03 - Fysiek Zwembadtraining Lebbeke' },
 		{ value: '2015-04-04-fysiek-zwembadtraining-lebbeke', label: '2015-04-04 - Fysiek Zwembadtraining Lebbeke' },
+		{ value: '2014-01-01 - Fysiek - Zwembadtraining - Lebbeke', label: '2014-01-01 - Fysiek Zwembadtraining Lebbeke' },		
+		{ value: '2014-01-02 - Fysiek - Zwembadtraining - Lebbeke', label: '2014-01-02 - Fysiek Zwembadtraining Lebbeke' },
+		{ value: '2014-01-03 - Fysiek - Zwembadtraining - Lebbeke', label: '2014-01-03 - Fysiek Zwembadtraining Lebbeke' },		
 		{ value: 'mtl_2015-04-05', label: '2015-04-05 - Techniek Zwembadtraining Lebbeke' },
 		{ value: 'mtl_2015-04-06', label: '2015-04-06 - Techniek Zwembadtraining Lebbeke' },		
 		{ value: 'mtl_2015-04-07', label: '2015-04-07 - Techniek Zwembadtraining Lebbeke' },
@@ -45,6 +49,34 @@ Enquiry.schema.post('save', function() {
 	}
 });
 
+Enquiry.schema.methods.checkDoubleEnquiries = function(callback) {
+	
+	if ('function' !== typeof callback) {
+		callback = function() {};
+	}
+	
+	var enquiry = this;
+	
+	console.log('Check 4 doubles - email: %s', req.user.email); // 2DO: req not known here !!! --> won't work
+	
+	keystone.list('User').model.find().where('isAdmin', true).exec(function(err, admins) {
+		
+		if (err) return callback(err);
+		
+		new keystone.Email('enquiry-notification').send({
+			to: admins,
+			from: {
+				name: 'hvanbelle',
+				email: 'contact@hvanbelle.com'
+			},
+			subject: 'New Enquiry for hvanbelle',
+			enquiry: enquiry
+		}, callback);
+		
+	});
+	
+};
+
 Enquiry.schema.methods.sendNotificationEmail = function(callback) {
 	
 	if ('function' !== typeof callback) {
@@ -53,9 +85,14 @@ Enquiry.schema.methods.sendNotificationEmail = function(callback) {
 	
 	var enquiry = this;
 	
-	keystone.list('User').model.find().where('isAdmin', true).exec(function(err, admins) {
+	// 2DO: search enquiries collection 4 doubles
+	keystone.list('User').model.find().where('isAdmin', true).exec(function(err, admins) { 
 		
 		if (err) return callback(err);
+		
+		// 2DO: Get all except last posted and change status of all except last
+		
+		
 		
 		new keystone.Email('enquiry-notification').send({
 			to: admins,
